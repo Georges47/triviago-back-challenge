493ec6e9d2d5e46bed2a9009ea7b8edaf68cedd8
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2023-08-03 21:14:42 -0300

    comit cambios controller

--- src/main/java/austral/triviago/triviagobackchallenge/presentation/controller/MessageController.java
+++ src/main/java/austral/triviago/triviagobackchallenge/presentation/controller/MessageController.java
@@ -5,11 +5,17 @@ import austral.triviago.triviagobackchallenge.persistence.domain.Message;
 import austral.triviago.triviagobackchallenge.presentation.dto.MessageFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@CrossOrigin
 public class MessageController {
 
     private final MessageService messageService;
@@ -19,22 +25,23 @@ public class MessageController {
     }
 
     @PostMapping("/messages")
-    public Message createMessage(String author, String content) {
-        return messageService.saveMessage(author, content);
+    public Message createMessage(@RequestBody Message message) {
+        return messageService.saveMessage(message.getAuthor(), message.getContent());
     }
 
     // The attributes of the message filter should be passed as query parameters
+    @GetMapping("/messages")
     public Page<Message> getMessages(@ModelAttribute MessageFilter filter, Pageable pageable) {
         return messageService.findAll(filter, pageable);
     }
 
     @GetMapping("/messages/{id}")
-    public Message getMessage(Long id) {
+    public Message getMessage(@PathVariable Long id) {
         return messageService.findById(id);
     }
 
     @DeleteMapping("/messages/{id}")
-    public Message deleteMessage(Long id) {
+    public Message deleteMessage(@PathVariable Long id) {
         return messageService.delete(id);
     }
 
